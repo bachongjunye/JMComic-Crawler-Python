1e8395e62ba1e2952faeddee0915655d44ff02eb
bachongjunye <3431361710@qq.com>
2025-03-24 20:49:17 +0800

    Update workflow_download.py
    111

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -4,10 +4,9 @@ from jmcomic.cl import JmcomicUI
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = str_to_list('''
 404482
-https://18comic.vip/album/404482/
 ''')
 
-download_album(ls)
+download_album(jm_albums)
 
 
 # 单独下载章节
